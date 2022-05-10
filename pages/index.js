00f77e2c40ea706307f77c01231fb0cdade86bbd
Brian Cox <brian@therealbriancox.com>
2022-05-10 04:02:55 -0700

    Adds Step 2 mock

--- pages/index.js
+++ pages/index.js
@@ -135,28 +135,65 @@ export default function Example() {
 
       {/* Step 1: Choose your video */}
       <div className="mx-auto max-w-7xl pt-6 pb-16 sm:pb-24 lg:pb-32">
-        <span className="block text-2xl font-bold text-gray-700">
+        <span className="block  mb-8 text-2xl font-bold text-gray-700">
           Step 1: Choose your video
         </span>
-        <ul role="list" className="mx-auto mt-8 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-        {files.map((file) => (
-          <li key={file.source} className="relative">
-            <div className="m-0 p-0 rounded-lg focus-within:ring-2 ring-offset-4 ring-orange-500">
-              <div className="relative aspect-video group block rounded-lg overflow-hidden">
-                <img src={file.source} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
-                <button type="button" className="absolute inset-0 focus:outline">
-                  <span className="sr-only">View details for {file.title}</span>
-                </button>
+        <ul role="list" className="mx-auto grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+          {files.map((file) => (
+            <li key={file.source} className="relative">
+              <div className="m-0 p-0 rounded-lg focus-within:ring-2 ring-offset-4 ring-orange-500">
+                <div className="relative aspect-video group block rounded-lg overflow-hidden">
+                  <img src={file.source} alt="" className="object-cover pointer-events-none group-hover:opacity-75" />
+                  <button type="button" className="absolute inset-0 focus:outline">
+                    <span className="sr-only">View details for {file.title}</span>
+                  </button>
+                </div>
+                <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{file.title}</p>
+                <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.size}</p>
               </div>
-              <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">{file.title}</p>
-              <p className="block text-sm font-medium text-gray-500 pointer-events-none">{file.size}</p>
-            </div>
-             
-            
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
       </div>
+
+      {/* Step 2: Upload your brand image */}
+      <div className="mx-auto max-w-7xl pt-6 pb-16 sm:pb-24 lg:pb-32">
+        <span className="block mb-8 text-2xl font-bold text-gray-700">
+          Step 2: Upload your brand image
+        </span>
+        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+          <div className="space-y-1 text-center">
+            <svg
+              className="mx-auto h-12 w-12 text-gray-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <div className="flex justify-center text-center text-sm text-gray-600">
+              <label
+                htmlFor="file-upload"
+                className="relative cursor-pointer bg-white rounded-md font-medium text-orange-600 hover:text-orange-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+              </label>
+              <p className="pl-1">or drag and drop</p>
+            </div>
+            <p className="text-xs text-center text-gray-500">1920x1080 (Powerpoint slide size) is best</p>
+            <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
