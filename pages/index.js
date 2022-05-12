1d0aa837540fe58a5d5d1dd9aba9ad0cd3f70253
Brian Cox <brian@therealbriancox.com>
2022-05-12 03:45:26 -0700

    Lints

--- pages/index.js
+++ pages/index.js
@@ -141,7 +141,7 @@ export default function Example() {
             <li key={video.youtubeUrl} className="relative">
               <div className="m-0 p-0 rounded-lg focus-within:ring-2 ring-offset-4 ring-orange-500">
                 <div className="relative aspect-video group block rounded-lg overflow-hidden">
-                  <iframe width="296" height="168" src={`https://www.youtube.com/embed/${video.ID}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls={0}></iframe>
+                  <iframe width="296" height="168" src={`https://www.youtube.com/embed/${video.ID}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen controls={0}></iframe>
                   <button type="button" className="absolute inset-0 focus:outline">
                     <span className="sr-only">View details for {video.name}</span>
                   </button>
@@ -211,9 +211,9 @@ export default function Example() {
             type="button"
             className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-orange-700 bg-orange-100 hover:bg-orange-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
           >
-            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-orange" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-orange" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
             Editing video...
           </button>)
