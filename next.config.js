29f04c6f4f533d3d45c3c5e04ba854b310de3168
Brian Cox <brian@therealbriancox.com>
2022-05-13 02:36:52 -0700

    Adds GitHub pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/next-video-brander',
+  assetPrefix: '/next-video-brander'
 }
 
 module.exports = nextConfig
