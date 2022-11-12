2ae7575ee6ab42237047189028fa2a9e02301116
Brian Cox <brian@therealbriancox.com>
2022-11-11 16:16:42 -0800

    Deploy gh-pages

--- _next/static/iSzr8-PJBXwuAe1nTShTv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/549-94e6908745dca805.js","static/chunks/pages/index-18e626fa93d425a8.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
